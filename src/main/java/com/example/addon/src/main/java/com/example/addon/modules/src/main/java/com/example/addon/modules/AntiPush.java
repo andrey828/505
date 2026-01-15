7d7548b8c710e8260865475872601ed87c49803a
andrey828 <andreymanea03@gmail.com>
2026-01-15 23:30:01 +0100

    Create AntiPush.java

---
+++ src/main/java/com/example/addon/src/main/java/com/example/addon/modules/src/main/java/com/example/addon/modules/AntiPush.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.AddonTemplate;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AntiPush extends Module {
+    public AntiPush() {
+        super(AddonTemplate.GOLDEN, "anti-push", "Evita empujes.");
+    }
+}
