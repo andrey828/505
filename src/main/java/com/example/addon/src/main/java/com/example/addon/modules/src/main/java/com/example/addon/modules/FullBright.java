b166673f504da65a3c9de88296ccb2a4643ef181
andrey828 <andreymanea03@gmail.com>
2026-01-15 23:59:03 +0100

    Update FullBright.java

--- src/main/java/com/example/addon/src/main/java/com/example/addon/modules/src/main/java/com/example/addon/modules/FullBright.java
+++ src/main/java/com/example/addon/src/main/java/com/example/addon/modules/src/main/java/com/example/addon/modules/FullBright.java
@@ -1,1 +1,12 @@
+package com.example.addon.modules;
 
+import com.example.addon.AddonTemplate;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class FullBright extends Module {
+    public FullBright() {
+        super(AddonTemplate.GOLDEN, "full-bright", "Luz total.");
+    }
+    @Override public void onActivate() { mc.options.getGamma().setValue(100.0); }
+    @Override public void onDeactivate() { mc.options.getGamma().setValue(1.0); }
+}
